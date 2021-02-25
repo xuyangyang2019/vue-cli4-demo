712fc6f3243fe08fe8a50888c1bd7bead74715f3
xuyangyang2019 <15268296911@163.com>
2021-02-25 18:25:19 +0800

    reset样式

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,16 @@
 </template>
 
 <style lang="scss">
+// 自定义的reset
+@import './styles/reset.css';
+// 排版样式
+// @import './styles/typo.css';
+// 适应不同浏览器
+// @import './styles/normalize.css';
+
+// 公共的css
+@import './styles/common.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -40,11 +50,40 @@
   }
 }
 
+// iconfont svg
 .icon {
   width: 1em;
   height: 1em;
   vertical-align: -0.15em;
   fill: currentColor;
   overflow: hidden;
+}
+
+//手机端
+@media screen and (max-width: 767px) {
+  #app {
+    border: solid red 1px;
+  }
+}
+
+//平板
+@media screen and (min-width: 768px) {
+  #app {
+    border: solid yellow 1px;
+  }
+}
+
+//小屏幕pc端
+@media screen and (min-width: 992px) {
+  #app {
+    border: solid blue 1px;
+  }
+}
+
+//大屏幕pc端
+@media screen and (min-width: 1200px) {
+  #app {
+    border: solid black 1px;
+  }
 }
 </style>
