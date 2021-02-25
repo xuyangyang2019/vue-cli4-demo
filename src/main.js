aecf136e6fb27ac768f5dd162100cfa027dc2f13
xuyangyang2019 <15268296911@163.com>
2021-02-25 18:12:22 +0800

    全局的过滤器

--- src/main.js
+++ src/main.js
@@ -11,7 +11,14 @@ import './assets/iconfont/iconfont.css'
 // 引入element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 全局的过滤器
+import filters from './filters'
+
 Vue.use(ElementUI)
+// 全局过滤器
+Object.keys(filters).forEach((filterName) => {
+  Vue.filter(filterName, filters[filterName])
+})
 
 Vue.config.productionTip = false
 
